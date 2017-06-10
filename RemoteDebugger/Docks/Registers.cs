dae6352d6be0aeaa2df18b7213c0d207fb07e257
Savoury SnaX <savoury.snax@googlemail.com>
2017-06-10 13:04:33 +0100

    Add vaidation to register input
    Always assumes hexidecimal input

--- RemoteDebugger/Docks/Registers.cs
+++ RemoteDebugger/Docks/Registers.cs
@@ -24,6 +24,7 @@ SOFTWARE.
 
 */
 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -34,18 +35,52 @@ namespace RemoteDebugger
 {
     public partial class Registers : WeifenLuo.WinFormsUI.Docking.DockContent
     {
+        delegate bool RegisterValidate(string toValidate,out string error);
         BindingList<RegisterItem> registerData;
         Dictionary<string, int> nameMap;
         List<Regex> regexList;
+        List<RegisterValidate> validateList;
 
         string viewName;
 
-        void InitialiseRegister(string name, string value,string regex)
+        void InitialiseRegister(string name, string value,string regex, RegisterValidate validator)
         {
             registerData.Add(new RegisterItem() { Register = name, Value = value });
             regexList.Add(new Regex(regex));
             nameMap.Add(name, registerData.Count - 1);
+            validateList.Add(validator);
         }
+
+        bool VOneByte(string input, out string error)
+        {
+            error = "";
+            int num = Convert.ToInt32(input, 16);
+            if (num<0 || num>255)
+            {
+                error = "Hexadecimal input out of range (00-FF)";
+                return false;
+            }
+            return true;
+        }
+
+        bool VTwoByte(string input, out string error)
+        {
+            error = "";
+            int num = Convert.ToInt32(input, 16);
+            if (num<0 || num>65535)
+            {
+                error = "Hexadecimal input out of range (0000-FFFF)";
+                return false;
+            }
+            return true;
+        }
+
+        bool VUnhandled(string input, out string error)
+        {
+            error="Sorry, currently unhandled - use set-register";
+            return false;
+        }
+
         public Registers(string name, string viewname)
         {
             viewName = viewname;
@@ -54,37 +89,38 @@ namespace RemoteDebugger
             registerData = new BindingList<RegisterItem>();
             regexList = new List<Regex>();
             nameMap = new Dictionary<string, int>();
+            validateList = new List<RegisterValidate>();
 
-            InitialiseRegister("A", "00", @"A\s*=\s*([a-fA-F0-9]{2})\s+");
-            InitialiseRegister("F", "00", @"F\s*=\s*([SZ5P3HNC ]+)\s+");
-            InitialiseRegister("BC", "0000", @"BC\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("DE", "0000", @"DE\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("HL", "0000", @"HL\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("IX", "0000", @"IX\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("IY", "0000", @"IY\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("SP", "0000", @"SP\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("PC", "0000", @"PC\s*=\s*([a-fA-F0-9]{4})\s+");
+            InitialiseRegister("A", "00", @"A\s*=\s*([a-fA-F0-9]{2})\s+",VOneByte);
+            InitialiseRegister("F", "00", @"F\s*=\s*([SZ5P3HNC ]+)\s+",VUnhandled);
+            InitialiseRegister("BC", "0000", @"BC\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("DE", "0000", @"DE\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("HL", "0000", @"HL\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("IX", "0000", @"IX\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("IY", "0000", @"IY\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("SP", "0000", @"SP\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("PC", "0000", @"PC\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
 
-            InitialiseRegister("I", "00", @"I\s*=\s*([a-fA-F0-9]{2})\s+");
-            InitialiseRegister("R", "00", @"R\s*=\s*([a-fA-F0-9]{2})\s+");
+            InitialiseRegister("I", "00", @"I\s*=\s*([a-fA-F0-9]{2})\s+",VOneByte);
+            InitialiseRegister("R", "00", @"R\s*=\s*([a-fA-F0-9]{2})\s+",VOneByte);
 
-            InitialiseRegister("MEMPTR", "0000",@"MEMPTR\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("INT", "DI", @"\s+(DI|EI)\s+");
-            InitialiseRegister("IMode", "IM1", @"\s+(IM0|IM1|IM2)\s+");
-            InitialiseRegister("VPS", "0", @"VPS\s*:\s*(\w)");
+            InitialiseRegister("MEMPTR", "0000",@"MEMPTR\s*=\s*([a-fA-F0-9]{4})\s+",VUnhandled);
+            InitialiseRegister("INT", "DI", @"\s+(DI|EI)\s+",VUnhandled);
+            InitialiseRegister("IMode", "IM1", @"\s+(IM0|IM1|IM2)\s+",VUnhandled);
+            InitialiseRegister("VPS", "0", @"VPS\s*:\s*(\w)",VUnhandled);
 
-            InitialiseRegister("A'", "00", @"A\'\s*=\s*([a-fA-F0-9]{2})\s+");
-            InitialiseRegister("F'", "00", @"F\'\s*=\s*([SZ5P3HNC ]+)\s+");
-            InitialiseRegister("BC'", "0000", @"BC\'\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("DE'", "0000", @"DE\'\s*=\s*([a-fA-F0-9]{4})\s+");
-            InitialiseRegister("HL'", "0000", @"HL\'\s*=\s*([a-fA-F0-9]{4})\s+");
+            InitialiseRegister("A'", "00", @"A\'\s*=\s*([a-fA-F0-9]{2})\s+",VOneByte);
+            InitialiseRegister("F'", "00", @"F\'\s*=\s*([SZ5P3HNC ]+)\s+",VUnhandled);
+            InitialiseRegister("BC'", "0000", @"BC\'\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("DE'", "0000", @"DE\'\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
+            InitialiseRegister("HL'", "0000", @"HL\'\s*=\s*([a-fA-F0-9]{4})\s+",VTwoByte);
 
             // Add registers
             dataGridView1.DataSource = registerData;
 
             dataGridView1.Columns[0].ReadOnly = true;
             dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = true;
             dataGridView1.CausesValidation = true;
         }
         override protected string GetPersistString()
@@ -155,7 +191,12 @@ namespace RemoteDebugger
                 string cValue = dataGridView1.Rows[e.RowIndex].Cells[1].Value as string;
                 if (cValue != e.FormattedValue.ToString())
                 {
-                    Program.telnetConnection.SendCommand("set-register " + regName + "=" + e.FormattedValue.ToString(), null);
+                    string err;
+                    if (validateList[e.RowIndex](e.FormattedValue.ToString(), out err))
+                    {
+                        Program.telnetConnection.SendCommand("set-register " + regName + "=" + e.FormattedValue.ToString(), null);
+                    }
+                    dataGridView1.Rows[e.RowIndex].ErrorText = err;
                 }
             }
         }
